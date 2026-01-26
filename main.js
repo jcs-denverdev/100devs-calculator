3b623b732d6c7d84d591e1ff80865511a81c38a5
Colin Stanford <j.colin.stanford@gmail.com>
2026-01-26 09:52:03 -0700

    calculator constructor given buttons object

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 
 // JS OOP BEST PRACTICES
+
 function Calculator () {
+    this.buttons = {
+        1: document.getElementById('one'),
+        2: document.getElementById('two'),
+        3: document.getElementById('three'),
+        4: document.getElementById('four'),
+        5: document.getElementById('five'),
+        6: document.getElementById('six'),
+        7: document.getElementById('seven'),
+        8: document.getElementById('eight'),
+        9: document.getElementById('nine'),
+        0: document.getElementById('zero'),
+        "+": document.getElementById('add'),
+        "-": document.getElementById('subtract'),
+        "x": document.getElementById('multiply'),
+        "/": document.getElementById('divide'),
+        "=": document.getElementById('equals')
+    }
     this.read = function () {
         let display_text = document.getElementById('display_text');
         // this.a = 
