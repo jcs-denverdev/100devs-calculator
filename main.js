1225630f53e12f526985621c1a5fa9dcfbb683d5
Colin Stanford <j.colin.stanford@gmail.com>
2026-04-19 11:14:09 -0600

    delete procedural js, start over using oop

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-function userPress() {
-    
-}
+// JS OOP BEST PRACTICES
 
-function displayNum() {
-    document.getElementById('display-text').innerText += 7;
-}
-
-// get buttons to click
-let h3 = document.querySelector('.button');
-h3.addEventListener('click', displayNum);
