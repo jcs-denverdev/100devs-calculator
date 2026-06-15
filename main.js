f161fbde1fff8f17b7dff4cdf0790d90c3482628
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-15 13:59:26 -0600

    calculator class created with methods

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // JS OOP BEST PRACTICES
 
+class Calculator {
+    constructor () {
+
+    }
+    add() {
+        console.log("Adding numbers")
+    }
+    subtract() {
+        console.log("Subtracting numbers")
+    }
+    multiply() {
+        console.log("Multiplying numbers")
+    }
+    divide() {
+        console.log("Dividing numbers")
+    }
+}
+
+let myCalculator = new Calculator();
