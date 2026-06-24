42733c2284731b185232b4e57a8ff3bfbfbf2c6e
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-24 15:39:18 -0600

    zero button displays zero except when it is the first character

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ let num_display = document.getElementById('display-text'); // variable for where
 // does each button need a click event?
     // each "button" is a div, with the class of "button" and an id to represent whatever number or symbol it is
 
+function addZeroToDisplay () {
+    if (num_display.innerText.length > 0) {
+        num_display.innerText += 0;
+    }
+}
 function addOneToDisplay () {
     num_display.innerText += 1;
 }
@@ -33,6 +38,9 @@ function addEightToDisplay () {
 function addNineToDisplay () {
     num_display.innerText += 9;
 }
+
+let zero = document.getElementById('zero');
+zero.addEventListener('click', addZeroToDisplay);
 
 let one = document.getElementById('one');
 one.addEventListener('click', addOneToDisplay);
