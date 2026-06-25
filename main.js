af7bef53fa35229a2d309d41e5656c00cebcf57f
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-25 15:11:14 -0600

    1 thru 9 all add to display

--- main.js
+++ main.js
@@ -6,23 +6,57 @@ let num_display = document.getElementById('display-text'); // variable for where
 // does each button need a click event?
     // each "button" is a div, with the class of "button" and an id to represent whatever number or symbol it is
 
-let seven = document.getElementById('seven');
-seven.addEventListener('click', addSevenToDisplay);
-
+function addOneToDisplay () {
+    num_display.innerText += 1;
+}
+function addTwoToDisplay () {
+    num_display.innerText += 2;
+}
+function addThreeToDisplay () {
+    num_display.innerText += 3;
+}
+function addFourToDisplay () {
+    num_display.innerText += 4;
+}
+function addFiveToDisplay () {
+    num_display.innerText += 5;
+}
+function addSixToDisplay () {
+    num_display.innerText += 6;
+}
 function addSevenToDisplay () {
     num_display.innerText += 7;
 }
+function addEightToDisplay () {
+    num_display.innerText += 8;
+}
+function addNineToDisplay () {
+    num_display.innerText += 9;
+}
+
+let one = document.getElementById('one');
+one.addEventListener('click', addOneToDisplay);
+
+let two = document.getElementById('two');
+two.addEventListener('click', addTwoToDisplay);
+
+let three = document.getElementById('three');
+three.addEventListener('click', addThreeToDisplay);
+
+let four = document.getElementById('four');
+four.addEventListener('click', addFourToDisplay);
+
+let five = document.getElementById('five');
+five.addEventListener('click', addFiveToDisplay);
+
+let six = document.getElementById('six');
+six.addEventListener('click', addSixToDisplay);
+
+let seven = document.getElementById('seven');
+seven.addEventListener('click', addSevenToDisplay);
 
 let eight = document.getElementById('eight');
 eight.addEventListener('click', addEightToDisplay);
 
-function addEightToDisplay () {
-    num_display.innerText += 8;
-}
-
 let nine = document.getElementById('nine');
 nine.addEventListener('click', addNineToDisplay);
-
-function addNineToDisplay () {
-    num_display.innerText += 9;
-}
