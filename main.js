ff17165c80e3042b88187d39121224a7c793e926
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-27 14:58:16 -0600

    clicking the 7 button displays a new 7 each time pressed

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // procedural js branch
 
-let num_display = document.getElementById('display-text');
-num_display.innerText = 5;
+let num_display = document.getElementById('display-text'); // variable for where you see the numbers as they are clicked
+// num_display.innerText = 5;
+
+// does each button need a click event?
+    // each "button" is a div, with the class of "button" and an id to represent whatever number or symbol it is
+
+let seven = document.getElementById('seven');
+seven.addEventListener('click', addSevenToDisplay);
+
+function addSevenToDisplay () {
+    num_display.innerText += 7;
+}
