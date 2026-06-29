cf3ca102ead237ae221c93dafc338549d5a8d200
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-29 13:19:43 -0600

    calculator class, switching branches to try procedural before oop

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
+
 // JS OOP BEST PRACTICES
 
 class Calculator {
-    constructor () {
-
+    constructor (name) {
+        this.name = name;
     }
     read() {
         // this.a = +prompt('a?', 0);
         // this.b = +prompt('b?', 0);
+
     }
     add() {
-        return this.a + this.b;
+        // return this.a + this.b;
+
     }
     subtract() {
-        return this.a - this.b;
+        // return this.a - this.b;
     }
     multiply() {
-        return this.a * this.b;
+        // return this.a * this.b;
     }
     divide() {
-        return this.a / this.b;
+        // return this.a / this.b;
     }
 }
 
-let myCalculator = new Calculator();
+let myCalculator = new Calculator("myCalculator"); // new object created
