d9d75837710a12ba79d3d50cc54926d738f0efde
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-29 14:14:57 -0600

    deleted class for branch test

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
 // procedural js branch
 
 
-// JS OOP BEST PRACTICES
-
-class Calculator {
-    constructor (name) {
-        this.name = name;
-    }
-    read() {
-        // this.a = +prompt('a?', 0);
-        // this.b = +prompt('b?', 0);
-
-    }
-    add() {
-        // return this.a + this.b;
-
-    }
-    subtract() {
-        // return this.a - this.b;
-    }
-    multiply() {
-        // return this.a * this.b;
-    }
-    divide() {
-        // return this.a / this.b;
-    }
-}
-
-let myCalculator = new Calculator("myCalculator"); // new object created
