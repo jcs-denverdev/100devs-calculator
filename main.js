cf23ed308ef9066b2fb676967e040e658a52cf52
Colin Stanford <j.colin.stanford@gmail.com>
2026-07-03 10:21:45 -0600

    read method returned something but idk, oop isnt computing

--- main.js
+++ main.js
@@ -2,14 +2,12 @@
 // JS OOP BEST PRACTICES
 function Calculator () {
     this.read = function () {
-        // this.a = +prompt('a?', 0);
-        // this.b = +prompt('b?', 0);
-        let display = document.getElementById('display-text');
-        
+        let display_text = document.getElementById('display_text');
+        // this.a = 
+        // display_text.innerText += this.a;
     }
     this.add = function () {
         return this.a + this.b;
-
     }
     this.subtract = function () {
         return this.a - this.b;
@@ -18,9 +16,12 @@ function Calculator () {
         return this.a * this.b;
     }
     this.divide = function () {
+        if (this.b == 0) throw new Error('div by 0 not allowed');
         return this.a / this.b;
     }
 }
 
 
 let calculator = new Calculator(); // new object created
+
+console.log(calculator.add());
